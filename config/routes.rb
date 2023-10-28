ec02e5f622c264afb8b80bcb27caa047fa90cd85
brianjdion <bdion@colgate.edu>
2023-10-28 14:14:24 -0400

    dashboard changes and testing work

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root 'dashboards#index'
 
   resources :reviews, :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_23_204628) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_28_174709) do
+  create_table "dashboards", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.datetime "published"
     t.string "timestamps"
