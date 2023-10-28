0208c171f9e0eb10bb6a3112e3566d3ab6e248a6
“Jaanhvi <jagarwal@colgate.edu>
2023-10-28 15:42:09 -0400

    commit user profile associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ApplicationRecord
+    belongs_to :profile
+
 end
---
+++ db/migrate/20231028180605_create_profiles.rb
@@ -0,0 +1,11 @@
+class CreateProfiles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :profiles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.text :bio
+      t.string :profile_photo
+
+      t.timestamps
+    end
+  end
+end
