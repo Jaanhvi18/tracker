fa914b43bd90bb0c25fbf19f46f99b0da6fd870b
“Jaanhvi <jagarwal@colgate.edu>
2023-10-28 23:59:00 -0400

    user model generated and user-profile association defined

---
+++ db/migrate/20231029035820_add_user_ref_to_profiles.rb
@@ -0,0 +1,5 @@
+class AddUserRefToProfiles < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :profiles, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_28_193725) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_29_035700) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -60,6 +60,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_28_193725) do
     t.integer "rating"
     t.datetime "published"
     t.string "timestamps"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
