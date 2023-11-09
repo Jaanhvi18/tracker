64906f7f882783e02c1bf09fcd8a352b8261fd42
“Jaanhvi <jagarwal@colgate.edu>
2023-11-08 20:49:27 -0500

    run git.sh with "source git.sh"

--- git.sh
+++ git.sh
@@ -1,5 +1,6 @@
 #! /bin/zsh
-export GITHUB_APP_ID="1c6625a9cb4a6834e127"
+
+export GITHUB_APP_ID="1c6625a9cb4a6834e127";
 echo "git app id exported!"
-export GITHUB_APP_SECRET="23109d667e6c816571c90ceaaccef8fbe54c26cf"
+export GITHUB_APP_SECRET="23109d667e6c816571c90ceaaccef8fbe54c26cf";
 echo "git secret exported!"
