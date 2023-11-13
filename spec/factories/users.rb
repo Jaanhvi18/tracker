919e09bcab3a5040460efeb97bd5b378a2838c98
brianjdion <bdion@colgate.edu>
2023-11-13 16:46:36 -0500

    create post button on dashboard now works

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,10 @@
 # In spec/factories/users.rb
 FactoryBot.define do
-    factory :user do
-      email { "test@example.com" }
-      password { "password" }
-      # Other attributes can be added here as needed
-    end
+  factory :user do
+    email { "test@example.com" }
+    password { "password" }
+    # Other attributes can be added here as needed
   end
+
+end
   
