003d1e0b20fec32214a473f6289176516ef1012d
“crisdj13” <cjimenez@colgate.edu>
2023-11-27 16:02:00 -0500

    trying to get Game Controller test working

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,3 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
-Review.create!(rating: "5")
-Post.create!(description: "Movie 1")
