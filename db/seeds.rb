2c0031e2f997b8fde8e7103a2d1bfbd0ebee952d
AValadez24 <avaladez@colgate.edu>
2023-11-28 17:09:24 -0500

    Seeding started

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+a = User.find_or_create_by!(email: 'alexadmin@example.com') do |user|
+    user.password = 'admin123'
+    user.password_confirmation = 'admin123'
+  end
+  
+b = User.find_or_create_by!(email: 'bobadmin@example.com') do |user|
+    user.password = 'admin123'
+    user.password_confirmation = 'admin123'
+  end
+  
+c = User.find_or_create_by!(email: 'caseyadmin@example.com') do |user|
+    user.password = 'admin123'
+    user.password_confirmation = 'admin123'
+  end
 
-Review.create!(rating: "5")
-Post.create!(description: "Movie 1")
+  movie_name = "FNAF"
+movie = Movie.find_or_create_by!(name: movie_name) do |m|
+  m.release_date = Date.today # Placeholder date
+  m.description = "Sample description"
+  m.duration = 120 # Placeholder duration
+  m.language = "English"
+  m.director = "Sample Director"
+end
+
+if movie.persisted?
+  puts "Movie created or found: #{movie.name}"
+else
+  puts "Failed to create or find movie with name: #{movie_name}"
+end
+  
+  # Create a post
+  post = Post.new(
+    user: a,
+    stars: 3,
+    description: 'lowkey kinda mid',
+    movie: movie
+  )
+  post.save!
