fa1bdc1a55476e909fa340b899562ae1504f57fc
“crisdj13” <cjimenez@colgate.edu>
2023-12-04 13:33:45 -0500

    added filters to rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,12 @@
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do 
+  add_filter 'spec'
+  add_filter 'config'
+  add_filter 'features'
+
+end
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
