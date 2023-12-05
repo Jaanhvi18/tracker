4b2a736311d8aa7031945e1a6980a1d2d75a5abd
AValadez24 <avaladez@colgate.edu>
2023-12-05 16:40:25 -0500

    stars fixed :p

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,10 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  
+  def show
+    @post = Post.find params[:id]
+  end
 
 
   #  # DELETE /posts/1 or /posts/1.json
