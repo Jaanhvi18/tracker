8e352314485c8ee2907704f9b5c294f321b8e2c8
brianjdion <bdion@colgate.edu>
2023-12-08 13:27:06 -0500

    The Final Push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-    def new_session_path(scope)
-        new_user_session_path
-      end
+    # def new_session_path(scope)
+    #     new_user_session_path
+    #   end
     
     
 end
